a4cbc2f880ff94920c4665d837ff0b102a8006fb
archebaldo <zakharov90@gmail.com>
2023-12-21 17:56:42 +0300

    Add: loader for scss

--- config/build/build-loaders.ts
+++ config/build/build-loaders.ts
@@ -2,11 +2,16 @@
 import { type RuleSetRule } from "webpack";
 
 export const buildLoaders = (): RuleSetRule[] => {
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [`style-loader`, `css-loader`, `sass-loader`],
+  };
+
   const tsLoader = {
     test: /\.tsx?$/,
     use: `ts-loader`,
     exclude: /node_modules/,
   };
 
-  return [tsLoader];
+  return [scssLoader, tsLoader];
 };
--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,15 +1,21 @@
 // packages;
 import { useState } from "react";
 
+// styles;
+import "./counter.scss";
+
 export const Counter = () => {
   const [count, setCount] = useState<number>(0);
 
   return (
-    <div>
-      <p>
+    <div className="counter">
+      <p className="text">
         Count: <span>{count}</span>
       </p>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+      <button
+        className="button"
+        onClick={() => setCount((prevCount) => prevCount + 1)}
+      >
         inc
       </button>
     </div>
