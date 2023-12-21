0b4e68e854ab300ee69c087019c8510513aaa2f0
archebaldo <zakharov90@gmail.com>
2023-12-21 21:55:45 +0300

    Add: basic styles (reset, variables, themes (light/dark))

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 // packages;
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 
 // pages;
@@ -9,14 +9,28 @@ import { AboutLazy } from "../../pages/about/about.lazy";
 // styles;
 import cls from "./app.module.scss";
 
+enum Theme {
+  LIGHT = `light`,
+  DARK = `dark`,
+}
+
 export const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+    );
+  };
+
   return (
-    <div className={cls.app}>
+    <div className={`${cls.app} ${theme}`}>
       <h1>React App</h1>
       <div className={cls.links}>
         <Link to="/">Main page</Link>
         <Link to="/about">About page</Link>
       </div>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<MainLazy />} />
