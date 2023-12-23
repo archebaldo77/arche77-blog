99a61a2c6eda5c3f43bd8732fcc0af9d78d55f6e
archebaldo <zakharov90@gmail.com>
2023-12-23 16:13:08 +0300

    Add: component Button (shared layer)

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,35 @@
+// styles;
+import cls from "./button.module.scss";
+
+// functions;
+import { classNames } from "@/shared/lib";
+
+// types;
+import { type ButtonHTMLAttributes } from "react";
+
+export enum ButtonTheme {
+  CLEAR = `clear`,
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ButtonTheme;
+}
+
+export const Button = (props: ButtonProps) => {
+  const {
+    className,
+    children,
+    theme = ButtonTheme.CLEAR,
+    ...restProps
+  } = props;
+
+  return (
+    <button
+      className={classNames(cls.button, {}, [className, cls[theme]])}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+};
