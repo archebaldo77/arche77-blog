1b0cf5857b82d820d77eaca932770e341ac35f55
archebaldo <zakharov90@gmail.com>
2023-12-25 23:20:38 +0300

    Add: component Spinner (shared layer)

---
+++ src/shared/ui/spinner/spinner.tsx
@@ -0,0 +1,15 @@
+// functions;
+import { classNames } from "@/shared/lib";
+
+// styles;
+import cls from "./spinner.module.scss";
+
+interface SpinnerProps {
+  className?: string;
+}
+
+export const Spinner = (props: SpinnerProps) => {
+  const { className } = props;
+
+  return <div className={classNames(cls.spinner, {}, [className])}></div>;
+};
