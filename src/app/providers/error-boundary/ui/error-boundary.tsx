ed8cdf851255b6bf10a6a9d38e6a58b043df7920
archebaldo <zakharov90@gmail.com>
2023-12-28 20:23:01 +0300

    Add: component ErrorBoundary (app layer, providers)

---
+++ src/app/providers/error-boundary/ui/error-boundary.tsx
@@ -0,0 +1,40 @@
+import React, { type ErrorInfo } from 'react';
+
+interface ErrorBoundaryProps {
+  fallback: React.ReactNode;
+  children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): ErrorBoundaryState {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo): void {
+    console.log(error, info.componentStack);
+  }
+
+  render(): React.ReactNode {
+    const { hasError } = this.state;
+    const { children, fallback } = this.props;
+
+    if (hasError) {
+      return fallback;
+    }
+
+    return children;
+  }
+}
