a3d3db8ac0a319c4fb77912c1beff51960917bb2
archebaldo <zakharov90@gmail.com>
2023-12-29 22:39:28 +0300

    Add: dependency and config for react-testing-library

---
+++ config/jest/jest-mock-empty-component.tsx
@@ -0,0 +1,5 @@
+const jestMockEmptyComponent = (): JSX.Element => {
+  return <div className="mock-empty-component" />;
+};
+
+export default jestMockEmptyComponent;
--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,3 +1,5 @@
+import path from 'path';
+
 import type { Config } from 'jest';
 
 const config: Config = {
@@ -5,12 +7,15 @@ const config: Config = {
   moduleFileExtensions: [`ts`, `tsx`, `jsx`, `js`, `json`],
   moduleNameMapper: {
     '^@/(.*)$': `<rootDir>/src/$1`,
+    '\\.(s?css)$': `identity-obj-proxy`,
+    '\\.svg': path.resolve(__dirname, `jest-mock-empty-component.tsx`),
   },
   rootDir: `../../`,
   roots: [`<rootDir>`],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  testEnvironment: `jsdom`,
+  testMatch: [`/**/__tests__/**/*.[jt]s?(x)`, `**/?(*.)+(spec|test).[tj]s?(x)`],
+  testPathIgnorePatterns: [`\\\\node_modules\\\\`],
+  setupFilesAfterEnv: [`<rootDir>/config/jest/jest-setup.ts`],
 };
 
 export default config;
