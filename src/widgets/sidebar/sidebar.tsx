4e971fbe673e8baace630888c611333b9f270c8c
archebaldo <zakharov90@gmail.com>
2024-03-09 21:27:54 +0300

    Add: Sidebar (widgets)

---
+++ src/widgets/sidebar/sidebar.tsx
@@ -0,0 +1,41 @@
+// libs;
+import { useState } from 'react';
+
+// helpers;
+import { classNames } from '@/shared/lib';
+
+// widgets;
+import { ThemeSwitcher } from '@/widgets/theme-switcher';
+
+// shared;
+import { Button, ButtonTheme } from '@/shared/ui';
+
+// styles;
+import classes from './sidebar.module.scss';
+
+interface SidebarProps {
+  className?: string;
+}
+
+export const Sidebar = (props: SidebarProps): JSX.Element => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+  const { className = `` } = props;
+
+  return (
+    <div
+      className={classNames(
+        classes[`sidebar`],
+        { [classes[`collapsed`]]: collapsed },
+        [className]
+      )}
+    >
+      <Button
+        onClick={() => setCollapsed((prev) => !prev)}
+        theme={ButtonTheme.CLEAR}
+      >
+        {collapsed ? `>` : `<`}
+      </Button>
+      <ThemeSwitcher className={classNames(classes[`theme-switcher`])} />
+    </div>
+  );
+};
