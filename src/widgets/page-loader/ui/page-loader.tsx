046b65613a17fab9849b92d4918f53ead6432119
archebaldo <zakharov90@gmail.com>
2024-03-10 21:44:21 +0300

    Add: PageLoader (widgets)

---
+++ src/widgets/page-loader/ui/page-loader.tsx
@@ -0,0 +1,22 @@
+// helpers;
+import { classNames } from '@/shared/lib';
+
+// shared;
+import { Loader } from '@/shared/ui';
+
+// styles;
+import classes from './page-loader.module.scss';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export const PageLoader = (props: PageLoaderProps): JSX.Element => {
+  const { className = `` } = props;
+
+  return (
+    <div className={classNames(classes[`page-loader`], {}, [className])}>
+      <Loader />
+    </div>
+  );
+};
