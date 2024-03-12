fcb9807f1e1d71fad060c28c2e54d61568f14d2b
archebaldo <zakharov90@gmail.com>
2024-03-12 22:00:11 +0300

    Add: LangSwitcher (widgets)

---
+++ src/widgets/lang-switcher/ui/lang-switcher.tsx
@@ -0,0 +1,34 @@
+// libs;s
+import { useTranslation } from 'react-i18next';
+
+// helpers;
+import { classNames } from '@/shared/lib';
+
+// shared;
+import { Button, ButtonTheme } from '@/shared/ui';
+
+// styles;
+import classes from './lang-switcher.module.scss';
+
+interface LangSwitcherProps {
+  className?: string;
+}
+
+export const LangSwitcher = (props: LangSwitcherProps): JSX.Element => {
+  const { i18n, t } = useTranslation();
+  const { className = `` } = props;
+
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === `ru` ? `en` : `ru`);
+  };
+
+  return (
+    <Button
+      className={classNames(classes[`lang-switcher`], {}, [className])}
+      theme={ButtonTheme.CLEAR}
+      onClick={toggleLang}
+    >
+      {t(`Язык`)}
+    </Button>
+  );
+};
