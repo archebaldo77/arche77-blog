5127cef27b5ba5cf130f42cb67548e99910de858
archebaldo <zakharov90@gmail.com>
2024-07-02 13:43:47 +0300

    Add: theme (light, dark), reset, variables for styles

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,6 @@
 // libraries;
+import { Suspense, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
 
 // pages;
 import { MainLazy } from '../../pages/main/main.lazy';
@@ -9,13 +9,28 @@ import { AboutLazy } from '../../pages/about/about.lazy';
 // styles;
 import classes from './app.module.scss';
 
+enum Theme {
+  LIGHT = `light`,
+  DARK = `dark`,
+}
+
 export const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+    );
+  };
   return (
-    <div className={classes[`app`]}>
+    <div className={`${classes[`app`]} ${theme}`}>
       <div className={classes[`navigation`]}>
         <Link to='/'>Главная</Link>
         <Link to='/about'>О нас</Link>
       </div>
+      <button className={classes[`toggle`]} onClick={toggleTheme}>
+        Изменить тему
+      </button>
       <div className={classes[`page`]}>
         <Suspense fallback='Loading...'>
           <Routes>
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/app/app';
 
 // styles;
-import './style.scss';
+import './styles/style.scss';
 
 render(
   <BrowserRouter>
