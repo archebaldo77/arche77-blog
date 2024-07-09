28f1ecbe18fb9fbce4e200359757b5885b7b10c6
archebaldo <zakharov90@gmail.com>
2024-07-09 21:56:22 +0300

    feat: button (shared) - new themes, sizes, square

--- src/shared/ui/button/button.test.tsx
+++ src/shared/ui/button/button.test.tsx
@@ -5,16 +5,16 @@ import { screen } from '@testing-library/react';
 import { Button, ButtonTheme } from './button';
 
 // helpers;
-import { renderWithTranslation } from '@/shared/lib';
+import { renderComponent } from '@/shared/lib';
 
 describe(`Test suites for Button (shared)`, () => {
   test(`with <theme=clear> [props]`, () => {
-    renderWithTranslation(<Button theme={ButtonTheme.CLEAR}>Click Me</Button>);
+    renderComponent(<Button theme={ButtonTheme.CLEAR}>Click Me</Button>);
     expect(screen.getByTestId(`button`)).toHaveClass(`clear`);
   });
 
   test(`without props`, () => {
-    renderWithTranslation(<Button>Click Me</Button>);
+    renderComponent(<Button>Click Me</Button>);
     expect(screen.getByTestId(`button`)).toBeInTheDocument();
     expect(screen.getByTestId(`button`)).toHaveClass(`button`);
   });
--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -9,13 +9,22 @@ import type { ButtonHTMLAttributes, FC } from 'react';
 
 export enum ButtonTheme {
   CLEAR = `clear`,
-  PRIMARY = 'primary',
-  PRIMARY_INVERTED = `primary-inverted`,
+  CLEAR_INVERTED = `clear-inverted`,
+  BACKGROUND = `background`,
+  BACKGROUND_INVERTED = `background-inverted`,
+}
+
+export enum ButtonSize {
+  M = `m`,
+  L = `l`,
+  XL = `xl`,
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ButtonTheme;
+  size?: ButtonSize;
+  square?: boolean;
 }
 
 export const Button: FC<ButtonProps> = (props): JSX.Element => {
@@ -23,12 +32,18 @@ export const Button: FC<ButtonProps> = (props): JSX.Element => {
     className = ``,
     children,
     theme = ButtonTheme.CLEAR,
+    size = ButtonSize.M,
+    square = false,
     ...otherProps
   } = props;
 
   return (
     <button
-      className={classNames(classes[`button`], {}, [className, classes[theme]])}
+      className={classNames(
+        classes[`button`],
+        { [classes[`square`]]: square },
+        [className, classes[theme], classes[size]],
+      )}
       {...otherProps}
       data-testid="button"
     >
