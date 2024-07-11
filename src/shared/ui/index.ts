439feee033a10e8d309df0fa6f7473553ff95220
archebaldo <zakharov90@gmail.com>
2024-07-11 23:28:44 +0300

    feat: modal (shared)

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export { AppLink, AppLinkTheme } from './app-link/app-link';
 export { Button, ButtonTheme, ButtonSize } from './button/button';
 export { Spinner } from './spinner/spinner';
+export { Modal } from './modal/modal';
---
+++ src/shared/ui/modal/modal.tsx
@@ -0,0 +1,76 @@
+// libraries;
+import { useCallback, useEffect, useRef, useState, type FC } from 'react';
+
+// helpers;
+import { classNames } from '@/shared/lib';
+
+// styles;
+import classes from './modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  isOpen?: boolean;
+  onClose?: () => void;
+}
+
+export const Modal: FC<ModalProps> = (props): JSX.Element => {
+  const { className = ``, children, isOpen = false, onClose } = props;
+
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+  const timerRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener(`keydown`, onEscKeyDown);
+    }
+
+    return () => {
+      clearTimeout(timerRef.current);
+      window.removeEventListener(`keydown`, onEscKeyDown);
+    };
+  }, [isOpen, onkeydown]);
+
+  const closeHandler = useCallback((): void => {
+    if (onClose != null) {
+      setIsClosing(true);
+
+      timerRef.current = setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, 300);
+    }
+  }, [onClose]);
+
+  const contentClickHandler = (e: React.MouseEvent): void => {
+    e.stopPropagation();
+  };
+
+  const onEscKeyDown = useCallback(
+    (e: KeyboardEvent): void => {
+      if (e.key === `Escape`) {
+        closeHandler();
+      }
+    },
+    [closeHandler],
+  );
+
+  return (
+    <div
+      className={classNames(
+        classes[`modal`],
+        { [classes[`opened`]]: isOpen, [classes[`closing`]]: isClosing },
+        [className],
+      )}
+      onClick={closeHandler}
+    >
+      <div className={classNames(classes[`overlay`])}>
+        <div
+          className={classNames(classes[`content`])}
+          onClick={contentClickHandler}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
