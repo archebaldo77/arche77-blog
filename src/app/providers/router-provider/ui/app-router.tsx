80e1c0e9a1a928323f8e4aaf9b28df6a4d0f736c
archebaldo <zakharov90@gmail.com>
2025-01-06 23:30:26 +0300

    style: app-router (app) add class for layout

--- src/app/providers/router-provider/ui/app-router.tsx
+++ src/app/providers/router-provider/ui/app-router.tsx
@@ -3,14 +3,20 @@ import { Routes, Route } from 'react-router-dom';
 
 import { routerConfig } from '../config/router-config';
 
+import { classNames } from '@/shared/lib';
+
+import classes from './app-router.module.scss';
+
 export const AppRouter = (): JSX.Element => {
   return (
-    <Suspense fallback={`Loading...`}>
-      <Routes>
-        {Object.values(routerConfig).map(({ element, path }) => {
-          return <Route key={path} path={path} element={element} />;
-        })}
-      </Routes>
-    </Suspense>
+    <div className={classNames(classes[`page-content`])}>
+      <Suspense fallback={`Loading...`}>
+        <Routes>
+          {Object.values(routerConfig).map(({ element, path }) => {
+            return <Route key={path} path={path} element={element} />;
+          })}
+        </Routes>
+      </Suspense>
+    </div>
   );
 };
