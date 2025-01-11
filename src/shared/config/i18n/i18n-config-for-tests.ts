e1be85522130f15773d4a078ff7770366c999852
archebaldo <zakharov90@gmail.com>
2025-01-11 21:19:11 +0300

    fix: i18n configs for correct work translations

--- src/shared/config/i18n/i18n-config-for-tests.ts
+++ src/shared/config/i18n/i18n-config-for-tests.ts
@@ -1,5 +1,7 @@
-import i18nForTests from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
+const i18nForTests = i18n.createInstance();
 
 i18nForTests.use(initReactI18next).init({
   lng: `ru`,
--- src/shared/config/i18n/i18n-config.ts
+++ src/shared/config/i18n/i18n-config.ts
@@ -4,7 +4,9 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
+const i18nForProduction = i18n.createInstance();
+
+i18nForProduction
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
@@ -20,4 +22,4 @@ i18n
     },
   });
 
-export default i18n;
+export default { i18nForProduction };
