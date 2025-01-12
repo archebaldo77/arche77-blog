22e517768574426d6f8c0bcf9076845e2b5fbc1d
archebaldo <zakharov90@gmail.com>
2025-01-12 22:40:32 +0300

    feat: button (shared) add sizes and square props

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -12,9 +12,17 @@ export enum ButtonTheme {
   INVERTED_OUTLINE_PRIMARY = `inverted-outline-primary`,
 }
 
+export enum ButtonSize {
+  M = `size-m`,
+  L = `size-l`,
+  XL = `size-xl`,
+}
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ButtonTheme;
+  square?: boolean;
+  size?: ButtonSize;
 }
 
 export const Button: FC<ButtonProps> = (props): JSX.Element => {
@@ -23,13 +31,19 @@ export const Button: FC<ButtonProps> = (props): JSX.Element => {
     theme = ButtonTheme.CLEAR,
     className = ``,
     children,
+    square = false,
+    size = ButtonSize.M,
     ...restProps
   } = props;
 
   return (
     <button
       type={type}
-      className={classNames(classes[`button`], {}, [className, classes[theme]])}
+      className={classNames(
+        classes[`button`],
+        { [classes[`square`]]: square },
+        [className, classes[theme], classes[size]],
+      )}
       {...restProps}>
       {children}
     </button>
