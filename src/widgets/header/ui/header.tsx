f869e7cebb0a002525ed39fde419a9f80ef2adbb
archebaldo <zakharov90@gmail.com>
2025-01-14 23:20:43 +0300

    refactor: use modal in header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,3 +1,14 @@
+import { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+  Button,
+  ButtonTheme,
+  Modal,
+  ModalTheme,
+  ButtonSize,
+} from '@/shared/ui';
+
 import { classNames } from '@/shared/lib';
 
 import classes from './header.module.scss';
@@ -8,6 +19,28 @@ interface HeaderProps {
 
 export const Header = (props: HeaderProps): JSX.Element => {
   const { className = `` } = props;
+  const [isAuthModal, setIsAuthModal] = useState<boolean>(false);
+  const { t } = useTranslation();
 
-  return <header className={classNames(classes[`header`], {}, [className])} />;
+  const onToggle = useCallback(() => {
+    setIsAuthModal((isAuthModal) => !isAuthModal);
+  }, []);
+
+  return (
+    <header className={classNames(classes[`header`], {}, [className])}>
+      <Button
+        className={classNames(classes[`button-auth`])}
+        onClick={onToggle}
+        theme={ButtonTheme.CLEAR}
+        size={ButtonSize.L}>
+        {t(`Войти`)}
+      </Button>
+      <Modal
+        isOpen={isAuthModal}
+        onClose={onToggle}
+        theme={ModalTheme.INVERTED_PRIMARY}>
+        Content must be here!
+      </Modal>
+    </header>
+  );
 };
