582c17bdff2c1a0e2cfdaf1ba9e19e89592d857a
archebaldo <zakharov90@gmail.com>
2025-01-14 23:15:24 +0300

    feat: portal (shared)

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,3 +2,4 @@ export { AppLink, AppLinkTheme } from './app-link/app-link';
 export { Button, ButtonTheme, ButtonSize } from './button/button';
 export { Spinner } from './spinner/spinner';
 export { ErrorIndicator } from './error-indicator/error-indicator';
+export { Portal } from './portal/portal';
