10ecd12663fc03b33e7c548223c914c316a76203
archebaldo <zakharov90@gmail.com>
2025-01-17 22:31:11 +0300

    feat: update json-server for compatibility with custom api

---
+++ server/index.ts
@@ -0,0 +1,56 @@
+import fs from 'fs';
+import jsonServer from 'json-server';
+import path from 'path';
+
+interface User {
+  username: string;
+  password: string;
+}
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.resolve(__dirname, `db.json`));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+server.use(async (_req, _res, next) => {
+  await new Promise((res) => {
+    setTimeout(res, 800);
+  });
+
+  next();
+});
+
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: 'Authorization error' });
+  }
+
+  next();
+});
+
+server.post(`/login`, (req, res) => {
+  const { username, password } = req.body;
+
+  const db = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, `db.json`), `utf-8`),
+  );
+  const { users } = db;
+
+  const user = users.find(
+    (item: User) => item.username === username && item.password === password,
+  );
+
+  if (user) {
+    return res.json(user);
+  }
+
+  return res.status(403).json({ message: 'Authorization error' });
+});
+
+server.use(router);
+
+server.listen(8000, () => {
+  console.log(`JSON-Server started on 8000 PORT`);
+});
